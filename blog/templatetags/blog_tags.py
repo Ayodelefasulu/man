e1934a09024729c239bcc496081d2f6756bfd134
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-10 10:05:39 +0100

    retrieve list of latest posts in sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,4 +8,12 @@ register = template.Library()
 # You could register it with a different name using "@register.simple_tag(name='my_tag')"
 def total_posts():
     """ This function retrieves the number of posts created in the blog """
-    return Post.published.Count()
+    return Post.published.count()
+
+
+# This is the template that will be returned
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5): # The counts specifies the number of posts to display
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return ('latest_posts': latest_posts)
+
