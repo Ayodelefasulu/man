23d0bc99604a997da924a46e779194bda213cddd
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-11 04:08:36 +0100

    adding sitemap functionality

---
+++ blog/sitemaps.py
@@ -0,0 +1,14 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Post
+
+class PostSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        return Post.published.all()
+
+    def lastmod(self, obj):
+        # receives each object returned by items() &
+        # returns the last time the object was modified
+        return obj.updated
