a9426184459486850acfc123e8c7ce3e7c02d124
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-11 17:47:19 +0100

    postgres database migration successful

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 """
+
 # Swithcing to postgres
 DATABASES = {
     'default': {
@@ -99,7 +99,7 @@ DATABASES = {
         'HOST': config('DB_HOST'),
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
