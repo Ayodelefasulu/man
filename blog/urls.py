065bf3e6fee0ec9d5724ac0e9355cb381b454ee8
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-11 10:29:16 +0100

    Revert "adding rss feeds functionality and undoing postgres migration"
    This reverts commit b2404f660b1dd669e611d20204b7cd8bdc0d56c3.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -17,5 +16,4 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name="post_detail"),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
