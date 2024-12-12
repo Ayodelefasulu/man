c5ab4439210dd3986ec2da7547349a647e31bd89
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-12 05:00:49 +0100

    weighing queries

--- blog/views.py
+++ blog/views.py
@@ -171,7 +171,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body') #search vector class imported
+            search_vector = SearchVector('title', weight='A'
+            ) + SearchVector('body', weight='B') #search vector class imported
             search_query = SearchQuery(query) #search query class imported
             results = (
                 """
@@ -183,7 +184,7 @@ def post_search(request):
                 Post.published.annotate(
                     search=search_vector,
                     rank=SearchRank(search_vector, search_query)
-                ).filter(search=search_query).order_by('-rank')
+                ).filter(rank__gte=0.3).order_by('-rank')
             )
 
     return render(
